4c4b8472be4bbe6a81dfcb7f77038eae35365f95
nanodonan <luvite77@gmail.com>
2022-11-27 10:47:00 -0300

    Ajustando layout do canvas

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 /** @type {HTMLCanvasElement} */
-
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
 const CANVAS_WIDTH = 500
 const CANVAS_HEIGHT = 300
 
-class Bolinha{
-    constructor(x,y){
+class Bolinha {
+    constructor(x, y) {
         this.x = x
         this.y = y
     }
-    draw(){
+    draw() {
         console.log("THIS X e Y: ", this)
-        ctx.fillRect(this.x,this.y,30,30)
+        ctx.fillRect(this.x, this.y, 30, 30)
     }
 
 }
@@ -24,16 +23,16 @@ class Bolinha{
 ctx.font = "15px arial"
 //ctx.fillText("Hello World", 10,20)
 
-window.addEventListener("click", (e)=>{
- console.log("E: ",e)
- //ctx.fillText("X", e.x, e.y)
- const pos = getMousePos(canvas,e)
- console.log("POS: ",pos)
- const bolinha = new Bolinha(pos.x, pos.y)
- bolinha.draw()
+window.addEventListener("click", (e) => {
+    console.log("E: ", e)
+    //ctx.fillText("X", e.x, e.y)
+    const pos = getMousePos(canvas, e)
+    console.log("POS: ", pos)
+    const bolinha = new Bolinha(pos.x, pos.y)
+    bolinha.draw()
 })
 
-  function getMousePos(canvas, evt) {
+function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
         x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
