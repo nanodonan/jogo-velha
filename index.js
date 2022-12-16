1731160d6e55fe24c10ea7c631f645121cde8319
nanodonan <luvite77@gmail.com>
2022-12-16 01:31:35 -0300

    Criando HUD

--- index.js
+++ index.js
@@ -2,19 +2,40 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+/** @type {HTMLCanvasElement} */
+const canvas2 = document.getElementById("canvas2")
+const ctx2 = canvas2.getContext("2d")
+
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 600
-
-const pontosX = [70, 250, 420]
-const pontosY = [70, 250, 425]
-
-let jogador = 2
 
 const tabuleiro = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ]
+
+const pontosX = [70, 250, 430]
+const pontosY = [70, 250, 430]
+
+let jogador = 2
+let primeiraJogada = true
+
+function velhota() {
+    let deuVelha = false
+    let control = 0
+    for (let i = 0; i < tabuleiro.length; i++) {
+        for (let j = 0; j < tabuleiro[i].length; j++) {
+            if (tabuleiro[i][j] === 0) {
+                control = -1
+            }
+        }
+    }
+    if (control === 0) {
+        deuVelha = true;
+    }
+    return deuVelha;
+}
 
 class Bolinha {
     constructor(x, y) {
@@ -24,7 +45,7 @@ class Bolinha {
     draw() {
         ctx.strokeStyle = "blue"
         ctx.beginPath();
-        ctx.arc(this.x + 50, this.y + 50, 60, 0, 2 * Math.PI);
+        ctx.arc(this.x + 50, this.y + 50, 55, 0, 2 * Math.PI);
         ctx.stroke();
     }
 }
@@ -39,63 +60,110 @@ class Xis {
         ctx.strokeStyle = "red"
         drawLine({ x1: this.x, y1: this.y, x2: this.x + this.length, y2: this.y + this.length })
         drawLine({ x1: this.x, y1: this.y + this.length, x2: this.x + this.length, y2: this.y })
-
     }
 }
 
+function atualizaPlacar() {
+    ctx2.clearRect(0, 0, 400, 200)
+    ctx2.fillStyle = "black"
+    ctx2.font = "45px arial"
+    ctx2.fillText("PRÓXIMO:   ", 5, 50)
+    if (primeiraJogada) {
+        ctx2.fillStyle = "blue"
+        ctx2.font = "bold 60px arial"
+        ctx2.fillText("O", 240, 52)
+    } else if (jogador === 2) {
+        ctx2.fillStyle = "red"
+        ctx2.font = "bold 60px arial"
+        ctx2.fillText("X", 240, 52)
+    } else {
+        ctx2.fillStyle = "blue"
+        ctx2.font = "bold 60px arial"
+        ctx2.fillText("O", 240, 52)
+    }
+}
+
+
+
 function drawLine(line) {
     ctx.lineWidth = 10
-    ctx.beginPath();
-    ctx.moveTo(line.x1, line.y1);
-    ctx.lineTo(line.x2, line.y2);
-    ctx.stroke();
+    ctx.beginPath()
+    ctx.moveTo(line.x1, line.y1)
+    ctx.lineTo(line.x2, line.y2)
+    ctx.stroke()
 }
 
 const linhas = [
     {
         x1: CANVAS_WIDTH / 3,
-        y1: 50,
+        y1: 20,
         x2: CANVAS_WIDTH / 3,
-        y2: CANVAS_HEIGHT - 50
+        y2: CANVAS_HEIGHT - 20
     },
     {
         x1: CANVAS_WIDTH - (CANVAS_WIDTH / 3),
-        y1: 50,
+        y1: 20,
         x2: CANVAS_WIDTH - (CANVAS_WIDTH / 3),
-        y2: CANVAS_HEIGHT - 50
+        y2: CANVAS_HEIGHT - 20
     },
     {
-        x1: 50,
+        x1: 20,
         y1: CANVAS_HEIGHT / 3,
-        x2: CANVAS_WIDTH - 50,
+        x2: CANVAS_WIDTH - 20,
         y2: CANVAS_HEIGHT / 3
     },
     {
-        x1: 50,
+        x1: 20,
         y1: CANVAS_HEIGHT - (CANVAS_HEIGHT / 3),
-        x2: CANVAS_WIDTH - 50,
+        x2: CANVAS_WIDTH - 20,
         y2: CANVAS_HEIGHT - (CANVAS_HEIGHT / 3)
     }
 ]
-//desenhando o tabuleiro
-linhas.forEach(line => { drawLine(line) })
+
+function desenhaTabuleiro() {
+    ctx.strokeStyle = "black"
+    //desenhando o tabuleiro
+    linhas.forEach(line => { drawLine(line) })
+}
+desenhaTabuleiro()
+
 
 
 window.addEventListener("click", (e) => {
+    primeiraJogada = false
+    atualizaPlacar()
     const pos = getMousePos(canvas, e)
-    updateTabuleiro(pos)
     const posTab = pegaPosicaoClicada(pos)
-    
+    updateTabuleiro(pos)
+    const resultado = verificaGanhador(jogador)
+    if (resultado) {
+        ctx2.clearRect(0, 0, 400, 200)
+        ctx2.fillStyle = "black"
+        ctx2.font = "bold 45px arial"
+        ctx2.fillText("VENCEDOR   ", 10, 40)
+        ctx2.fillText("JOGADOR:   ", 10, 100)
+        if (jogador === 1) {
+            ctx2.fillStyle = "red"
+            ctx2.font = "bold 60px arial"
+            ctx2.fillText("X", 252, 102)
+        } else {
+            ctx2.fillStyle = "blue"
+            ctx2.font = "bold 60px arial"
+            ctx2.fillText("O", 252, 102)
+        }
+    }
+
     if (jogador === 1) {
         const xis = new Xis(pontosX[posTab.y], pontosY[posTab.x])
         xis.draw()
         jogador = 2
+
     } else {
         const bolinha = new Bolinha(pontosX[posTab.y], pontosY[posTab.x])
         bolinha.draw()
         jogador = 1
-    }
 
+    }
 })
 
 function getMousePos(canvas, evt) {
@@ -105,33 +173,20 @@ function getMousePos(canvas, evt) {
         y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
     };
 }
-/*function setCel(pos) {
-    const p = { ...pos }
-    switch (true) {
-        case (p.x <= 200 && p.y <= 200): p.x = 70; p.y = 70; break;
-        case (p.x <= 400 && p.y <= 200): p.x = 250; p.y = 70; break;
-        case (p.x <= 600 && p.y <= 200): p.x = 420; p.y = 70; break;
-        case (p.x <= 200 && p.y <= 400): p.x = 70; p.y = 250; break;
-        case (p.x <= 400 && p.y <= 400): p.x = 250; p.y = 250; break;
-        case (p.x <= 600 && p.y <= 400): p.x = 420; p.y = 250; break;
-        case (p.x <= 200 && p.y <= 600): p.x = 70; p.y = 425; break;
-        case (p.x <= 400 && p.y <= 600): p.x = 250; p.y = 425; break;
-        case (p.x <= 600 && p.y <= 600): p.x = 420; p.y = 425; break;
-        default: break;
-    }
-    return p
-}
-*/
 
 function updateTabuleiro(pos) {
     const posTab = pegaPosicaoClicada(pos)
-    tabuleiro[posTab.x][posTab.y] = 2
+    if (posTab.x != -1 && posTab.y != -1) {
+        if (tabuleiro[posTab.x][posTab.y] === 0) {
+            tabuleiro[posTab.x][posTab.y] = jogador
+        }
+    }
 }
-
 
 function pegaPosicaoClicada(pos) {
     let resultadoX = -1
     let resultadoY = -1
+
     const range = {
         0: [0, 200],
         1: [200, 400],
@@ -146,8 +201,77 @@ function pegaPosicaoClicada(pos) {
         if (pos.x > min && pos.x < max) {
             resultadoY = key
         }
-
     }
 
     return { x: resultadoX, y: resultadoY }
+}
+
+
+function pegarColuna(index) {
+    return tabuleiro.map(item => item[index])
+}
+
+function verificaGanhador(player = 2) {
+    let resultado = false
+
+    //verificar linha
+    for (let i = 0; i < tabuleiro.length; i++) {
+        const linha = tabuleiro[i];
+        const ganhouLinha = linha.every((item) => item === player)
+        if (ganhouLinha) {
+            ctx.strokeStyle = "green"
+            drawLine({ x1: 0, y1: pontosY[i] + 50, x2: CANVAS_WIDTH, y2: pontosY[i] + 50 })
+            return true
+        }
+    }
+
+    //verificar coluna
+    for (let i = 0; i < tabuleiro[0].length; i++) {
+        const coluna = pegarColuna(i)
+        const ganhouColuna = coluna.every((item) => item === player)
+        if (ganhouColuna) {
+            ctx.strokeStyle = "green"
+            drawLine({ x1: pontosX[i] + 50, y1: 0, x2: pontosX[i] + 50, y2: CANVAS_HEIGHT })
+            return true
+        }
+    }
+
+    //verificar diagonais
+    const diagonal1 = [tabuleiro[0][0], tabuleiro[1][1], tabuleiro[2][2]]
+    const diagonal2 = [tabuleiro[0][2], tabuleiro[1][1], tabuleiro[2][0]]
+    const gannhouDiagonal1 = diagonal1.every(item => item === player)
+    const gannhouDiagonal2 = diagonal2.every(item => item === player)
+
+    if (gannhouDiagonal1) {
+        ctx.strokeStyle = "green"
+        drawLine({ x1: 0, y1: 0, x2: CANVAS_WIDTH, y2: CANVAS_HEIGHT })
+        return true
+    } else if (gannhouDiagonal2) {
+        ctx.strokeStyle = "green"
+        drawLine({ x1: CANVAS_WIDTH, y1: 0, x2: 0, y2: CANVAS_HEIGHT })
+        return true
+    }
+    //verificar velha
+    const superVelha = velhota()
+    if (!resultado && superVelha) {
+        ctx2.clearRect(0, 0, 400, 200)
+        ctx2.fillStyle = "black"
+        ctx2.font = "bold 70px arial"
+        ctx2.fillText("VELHA!!",10, 100)
+    } else console.log("Não deu")
+    console.log(tabuleiro)
+    return resultado
+}
+
+
+atualizaPlacar()
+
+function clicou() {
+    for (let i = 0; i < tabuleiro.length; i++) {
+        for (let j = 0; j < tabuleiro[i].length; j++) {
+            tabuleiro[i][j] = 0
+        }
+    }
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    desenhaTabuleiro()
 }
--- todo.md
+++ todo.md
@@ -2,6 +2,9 @@ Tasks do jogo da velha
 X- desenhar quadro
 X- desenhar X e O no click
 X- identificar click jogador
-- identificar ganhador
-- atualizar hud
+X- melhorar desenho da velha
+X- identificar ganhador
+- se tem ganhador => resseta a tela toda
+X- atualizar hud (vez, ganhador/linha) => 
+X- verificar se preencheu todo tabuleiro
 
